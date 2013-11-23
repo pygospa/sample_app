7755545bd9a0fc6bdcfaa3f2c699767a8e86fd01
Kannan Thambiah <pygospa@gmail.com>
2013-11-23 10:11:15 +0100

    Refactor to remove description for it-blocks, as code is human-readable

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,53 +8,28 @@ describe "StaticPages" do
 
   describe "Home page" do
     before { visit root_path }
-
-    it "should have the content 'Sample App'" do
-      should have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      should have_title("#{base_title}")
-    end
-
-    it "should not hava a custom page title" do
-      should_not have_title(' | Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("#{base_title}") }
+    it { should_not have_title(' | Home') }
   end
 
 
   describe "Help page" do
     before { visit help_path }
-
-    it "should have the content 'Help'" do
-      should have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      should have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it "should have the contentn 'About Us'" do
-      should have_content('About Us')
-    end
+    it { should have_content('About Us') }
     
-    it "should have the title 'About Us'" do
-      should have_title("#{base_title} | About Us")
-    end
+    it { should have_title("#{base_title} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it "should have the content 'Contact'" do
-      should have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      should have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title("#{base_title} | Contact") }
   end
 end
