8c277f2c1431e065127eeb62827080d765a0a453
Kannan Thambiah <pygospa@gmail.com>
2013-11-23 10:02:52 +0100

    Refactor RSpec examples to make them DRY

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,55 +5,52 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before { visit root_path }
+    
     it "should have the content 'Sample App'" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not hava a custom page title" do
-      visit root_path
       expect(page).not_to have_title(' | Home')
     end
   end
 
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
+
     it "should have the contentn 'About Us'" do
-      visit about_path
       expect(page).to have_content('About Us')
     end
     
     it "should have the title 'About Us'" do
-      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the content 'Contact'" do
-      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
