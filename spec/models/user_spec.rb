ab3b555edb607229cbd7f9bf852731dd05846524
Kannan Thambiah <pygospa@gmail.com>
2013-12-02 22:04:38 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate)}
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
@@ -107,5 +108,10 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
